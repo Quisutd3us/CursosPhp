f9ae15aef7007ba2239af3571454b368c4bcaf80
David Salinas <dnarino@gmail.com>
2017-07-19 17:31:59 -0500

    terminadp Rap1

---
+++ Sena/HolaMundo.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>Hola Mundo</title>
+    </head>
+    <body>
+        <?php echo "<h1>Hola mundo Sena | php</h1>" ?>
+        <?php echo "<h2>David Nariño</h2>" ?>  
+        <?php echo "<h2>Curso Sena</h2>" ?>
+    </body>
+</html>
+
+
